3ad92c93e92cb5bde9438e7863a903052defcf7d
Md Akij Islam <mdakijislam23@gmail.com>
2024-07-03 01:43:25 +0600

    Update dex.js

--- js/dex.js
+++ js/dex.js
@@ -1,1 +1,6 @@
-3
+window.onload = function() {
+      setTimeout(function() {
+        document.getElementById('myIframe').classList.add('loaded');
+        document.querySelector('.loader-container').style.display = 'none';
+      }, 2000); 
+    };
