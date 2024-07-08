912b12b71aa8c6e25906401fb8ce0ca2a5807f24
Md Akij Islam <mdakijislam23@gmail.com>
2024-07-09 05:42:33 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
-// 霸都丶傲天 2019/7/9  https://Github.com/AJLoveChina
+
 var config = {
-    // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
+    // The following sentences do not have to be 7. You can change them to 4 or 2.
     lines: [
-        "亲爱的雯雯",
-        "花开一季，有落红为证",
-        "逝去的岁月，如何能托于烟尘",
-        "你的浅笑，是我永生眷恋",
-        "相思的真情，是否能明白一二",
-        "亲爱的宝贝，你是我的唯一",
-        "爱你永远。。。",
+        "Dear M",
+        "A season of flowers，There is red blood as evidence",
+        "The passing years，How can I support your smile amid the smoke and dust?",
+        "Your smile，It is eternal love ",
+        "Lovesickness，Can you understand one or two?",
+        "Dear Mumu, You are my only，love you forever一",
+        "。。。",
     ],
-    // 相爱的时间 记住格式不能写错了, 非常重要
-    // 格式一定要和下面这个时间格式一样
-    // 分别是 年-月-日T时时:分分:秒秒
-    //      温馨警告:
-    //          1.时分秒都必须是俩个数字  比如 00:00:00
-    //          2.T不能丢了
-    date: "2017-07-20T20:15:00",
-    // 双方的名字
-    names: ["笨笨熊", "黑猪猪"]
+    // The time of love, remember the format can not be wrong,Very important 
+    //The format must be the same as the time format below
+    // They are year-month-day T hour:minute:second
+    //      Friendly warning:
+    //          1.Hours, minutes and seconds must all be two digits, for example 00:00:00
+    //          2.T can't be lost
+    date: "2018-06-20T20:15:00",
+    // Names of both parties
+    names: ["Mamata", "Akij"]
 }
