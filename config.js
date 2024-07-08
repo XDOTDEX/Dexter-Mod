863794b6e91d02fb74ff17c063ea8de25d0bf69d
Md Akij Islam <mdakijislam23@gmail.com>
2024-07-09 05:43:28 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+
+var config = {
+    // The following sentences do not have to be 7. You can change them to 4 or 2.
+    lines: [
+        "Dear M",
+        "A season of flowers，There is red blood as evidence",
+        "The passing years，How can I support your smile amid the smoke and dust?",
+        "Your smile，It is eternal love ",
+        "Lovesickness，Can you understand one or two?",
+        "Dear Mumu, You are my only，love you forever一",
+        "。。。",
+    ],
+    // The time of love, remember the format can not be wrong,Very important 
+    //The format must be the same as the time format below
+    // They are year-month-day T hour:minute:second
+    //      Friendly warning:
+    //          1.Hours, minutes and seconds must all be two digits, for example 00:00:00
+    //          2.T can't be lost
+    date: "2018-06-20T20:15:00",
+    // Names of both parties
+    names: ["Mamata", "Akij"]
+}
